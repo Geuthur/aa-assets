788032d479eb58d3f262eb0a1aed9ead6534ebbf
Geuthur <geuthur@users.noreply.github.com>
2025-04-29 14:39:43 +0200

    [ADD] Cache Buster

---
+++ assets/constants.py
@@ -0,0 +1,9 @@
+"""
+Constants
+"""
+
+# Standard Library
+import os
+
+AA_ASSETS_BASE_DIR = os.path.join(os.path.dirname(__file__))
+AA_ASSETS_STATIC_DIR = os.path.join(AA_ASSETS_BASE_DIR, "static", "assets")
